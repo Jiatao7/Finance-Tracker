36c5a80b356dee72d9bd9bad9e17890dee8dc1c8
Jiatao Geng <jiataogeng7@gmail.com>
2025-01-03 15:53:28 -0500

    create server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-    
+require('dotenv').config()
+
+const express = require('express')
+
+// Express
+const app = express()
+
+// Middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({mssg: 'Welcome to the app'})
+})
+
+// Listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port', process.env.PORT)
+})
