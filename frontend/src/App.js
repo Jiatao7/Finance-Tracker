ac66b4a7503557a34c29e5fcf646419835fdbda2
Jiatao Geng <jiataogeng7@gmail.com>
2025-03-18 12:21:58 -0400

    Update routing for account setup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { useUserContext } from './context/UserContext'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Setup from './pages/Setup'
 import Transactions from './pages/Transactions'
 import Budget from './pages/Budget'
 import Statistics from './pages/Statistics'
@@ -14,20 +15,33 @@ import Navbar from './components/Navbar'
 function App() {
   const user = useUserContext().user
 
+  const determineDefaultPath = () => {
+    if(user) {
+      if(user.new) {
+        return <Navigate to="setup" />;
+      } else {
+        return <Home />
+      }
+    } else {
+      return <Navigate to="login" />;
+    }
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className='pages m-16'>
           <Routes>
-            <Route path='/' element={user ? <Home /> : <Navigate to="/login"/>} />
+            <Route path='/' element={determineDefaultPath()} />
             <Route path='/login' element={!user ? <Login /> : <Navigate to="/"/>} />
             <Route path='/signup' element={!user ? <Signup /> : <Navigate to="/"/>} />
-            <Route path='/transactions' element={user ? <Transactions /> : <Navigate to="/login"/>} />
-            <Route path='/budget' element={user ? <Budget /> : <Navigate to="/login"/>} />
-            <Route path='/statistics' element={user ? <Statistics /> : <Navigate to="/login"/>} />
+            <Route path='/setup' element={user && user.new ? <Setup /> : <Navigate to="/"/>} />
+            <Route path='/transactions' element={user && !user.new ? <Transactions /> : <Navigate to="/"/>} />
+            <Route path='/budget' element={user && !user.new ? <Budget /> : <Navigate to="/"/>} />
+            <Route path='/statistics' element={user && !user.new ? <Statistics /> : <Navigate to="/"/>} />
           </Routes>
-        </div>  
+        </div>
       </BrowserRouter>
     </div>
   );
---
+++ frontend/src/pages/Setup.js
@@ -0,0 +1,5 @@
+export default function Budget() {
+    return (
+        <h2>Setup</h2>
+    )
+}
