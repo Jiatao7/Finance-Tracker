c85d35cf973c8049e79604a408a3f442f5ccecde
Jiatao Geng <jiataogeng7@gmail.com>
2025-03-18 12:31:42 -0400

    Update navbar for account setup

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,6 +6,10 @@ const Navbar = () => {
   const { logout } = useLogout()
   const user = useUserContext().user
 
+  function displayWarning() {
+    alert("Please complete account setup first!")
+  }
+  
   function handleClick() {
     logout()
   }
@@ -16,20 +20,26 @@ const Navbar = () => {
         <Link to="/" className='font-semibold text-2xl'>Finance Tracker</Link>
       </div>
       <div className="links">
-        {user && (
+        {user && user.new && (
+          <>
+            <Link onClick={displayWarning} className='p-2 m-2 text-lg'>Transactions</Link>
+            <Link onClick={displayWarning} className='p-2 m-2 text-lg'>Statistics</Link>
+            <Link onClick={handleClick} className='p-2 m-2 text-lg cursor-pointer'>Log out</Link>
+          </>
+        )}
+        {user && !user.new && (
           <>
             <Link to="/transactions" className='p-2 m-2 text-lg'>Transactions</Link>
             <Link to="/statistics" className='p-2 m-2 text-lg'>Statistics</Link>
+            <Link onClick={handleClick} className='p-2 m-2 text-lg cursor-pointer'>Log out</Link>
           </>
         )}
-        {user ? (
-          <Link onClick={handleClick} className='p-2 m-2 text-lg cursor-pointer'>Log out</Link>
-        ) :
-          (<>
+        {!user && (
+          <>
             <Link to="/login" className='p-2 m-2 text-lg'>Log In</Link>
             <Link to="/signup" className='p-2 m-2 text-lg'>Sign Up</Link>
-          </>)
-        }        
+          </>
+        )}        
       </div>
     </nav>
   )
